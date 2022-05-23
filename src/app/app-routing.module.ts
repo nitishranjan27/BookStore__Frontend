6af7a1142c5d0371807ca2cbdde12d3d80061fbf
nitishranjan27 <nk47595@gmail.com>
2022-05-23 22:50:04 +0530

    Nitish Created And Modified AllPages Design ,All Api and Dashboard Created  in BookStore__Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { GetallbookComponent } from './getallbook/getallbook.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
 
 const routes: Routes = [
   {path:'register',component: RegisterComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path: 'forgot',component:ForgotpasswordComponent},
+  {path: 'reset/:token',component:ResetpasswordComponent},
+  {path:'', redirectTo:"/login", pathMatch:'full' },
+  {path:'dashboard',component:DashboardComponent,
+  children: [
+    { path: '', redirectTo: 'getallbook', pathMatch: 'full', },
+    { path: 'getallbook',component:GetallbookComponent}
+    ]},
 ];
 
 @NgModule({
